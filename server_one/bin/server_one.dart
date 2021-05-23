706ac8766be6867265c7208224b6d4548e637576
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-05-23 23:22:16 +0900

    initialize

---
+++ server_one/bin/server_one.dart
@@ -0,0 +1,5 @@
+import 'package:server_one/server_one.dart' as server_one;
+
+void main(List<String> arguments) {
+  print('Hello world: ${server_one.calculate()}!');
+}
---
+++ server_one/test/server_one_test.dart
@@ -0,0 +1,8 @@
+import 'package:server_one/server_one.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
